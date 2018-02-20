c4e46402ca7588418917be67ba51e58098ce2770
Trevor Garn <trevash77@gmail.com>
2018-02-20 13:45:35 -0700

    Added skeleton of schema for database

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE customer (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	
+);
+
+CREATE TABLE employee (
+	id INT AUTOINCREMENT PRIMARY KEY,
+	
+);
+
+CREATE TABLE administrator (
+	
+);
+
+CREATE TABLE warehouse (
+	name STRING PRIMARY KEY,
+	
+);
+
+CREATE TABLE item (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+);
+
+CREATE TABLE STORE (
+
+);
+
+CREATE TABLE SERVICE (
+
+);
+
+CREATE TABLE Reviews (
+
+);
+
+
