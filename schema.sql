097d5d7e985994f038e35fab73665cda75d98329
Trevor Garn <trevash77@gmail.com>
2018-03-10 20:12:26 -0700

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,36 +1,228 @@
-CREATE TABLE customer (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	
-);
+--
+-- Database: `IT350`
 
-CREATE TABLE employee (
-	id INT AUTOINCREMENT PRIMARY KEY,
-	
-);
+--
+-- Table structure for table `Account`
+--
 
-CREATE TABLE administrator (
-	
-);
+CREATE TABLE `Account` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `customerId` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE warehouse (
-	name STRING PRIMARY KEY,
-	
-);
+-- --------------------------------------------------------
 
-CREATE TABLE item (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-);
+--
+-- Table structure for table `Administrator`
+--
 
-CREATE TABLE STORE (
+CREATE TABLE `Administrator` (
+  `department` int(11) NOT NULL,
+  `employee id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-);
+-- --------------------------------------------------------
 
-CREATE TABLE SERVICE (
+--
+-- Table structure for table `Customer`
+--
 
-);
+CREATE TABLE `Customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Reviews (
+-- --------------------------------------------------------
 
-);
+--
+-- Table structure for table `Employee`
+--
 
+CREATE TABLE `Employee` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `position` varchar(30) NOT NULL,
+  `warehouse id` int(11) DEFAULT NULL,
+  `store id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Orders`
+--
+
+CREATE TABLE `Orders` (
+  `id` int(11) NOT NULL,
+  `delivery address` varchar(30) NOT NULL,
+  `total cost` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(4) NOT NULL,
+  `number purchased` int(6) NOT NULL,
+  `warehouse address` varchar(20) NOT NULL,
+  `number remaining` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product_Orders`
+--
+
+CREATE TABLE `Product_Orders` (
+  `order id` int(11) NOT NULL,
+  `product id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Service`
+--
+
+CREATE TABLE `Service` (
+  `service number` int(11) NOT NULL,
+  `date` varchar(8) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `store address` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Store`
+--
+
+CREATE TABLE `Store` (
+  `id` int(11) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `hours` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Warehouse`
+--
+
+CREATE TABLE `Warehouse` (
+  `address` varchar(30) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `hours` varchar(5) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Account`
+--
+ALTER TABLE `Account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Administrator`
+--
+ALTER TABLE `Administrator`
+  ADD PRIMARY KEY (`department`);
+
+--
+-- Indexes for table `Customer`
+--
+ALTER TABLE `Customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Employee`
+--
+ALTER TABLE `Employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Orders`
+--
+ALTER TABLE `Orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Service`
+--
+ALTER TABLE `Service`
+  ADD PRIMARY KEY (`service number`);
+
+--
+-- Indexes for table `Store`
+--
+ALTER TABLE `Store`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Warehouse`
+--
+ALTER TABLE `Warehouse`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Account`
+--
+ALTER TABLE `Account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Customer`
+--
+ALTER TABLE `Customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Employee`
+--
+ALTER TABLE `Employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Orders`
+--
+ALTER TABLE `Orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Service`
+--
+ALTER TABLE `Service`
+  MODIFY `service number` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Warehouse`
+--
+ALTER TABLE `Warehouse`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
